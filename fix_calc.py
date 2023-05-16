6cca990d7d6886a6b50f0cee291055324f836392
dbaugh <114878979+baughtnet@users.noreply.github.com>
2023-05-16 19:24:22 +0000

    initial commit

---
+++ fix_calc.py
@@ -0,0 +1,14 @@
+# ***THIS PROGRAM CONTAINS INTENTIONAL ERRORS***
+# ***TO EARN FULL MARKS, FIX THEM AND LET ME KNOW***
+# tells the user what the program does
+rint("This program will add 2 number together")
+
+# takes 2 numbers from the user and converts them to integers
+num = int(input("Please give me a number  "))
+num2 = input("Please give me a second number  ")
+
+# calculates the answer
+answer = num1 + num
+
+# prints the answer out on screen
+print(f"{num1} + {num2} = {answer}")
