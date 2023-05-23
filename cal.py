8bdb1eb89867948d45b8cc5a0ac3620d2b7fce56
dbaugh <114878979+baughtnet@users.noreply.github.com>
2023-05-23 17:29:26 +0000

    Added cal.py program

---
+++ cal.py
@@ -0,0 +1,18 @@
+# Your task is to take this program and change it 
+# so that it asks the user for the month and year
+# to create the calender
+# Bonus marks are awarded to programs that are either
+# more complex or access other functions from the
+# calendar library(google/chatGPT are your friends)
+
+# Python program to display calend or a given month of the year
+
+# import libraries
+import calendar
+
+# define month and year
+yy = 1999
+mm = 6
+
+# display the calendar
+print(calendar.month(yy, mm))
